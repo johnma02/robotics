5559f572815f319a02247cf03075e2e8097a4114
John <ma.jonathan02@gmail.com>
2023-03-04 17:57:00 +0000

    added some publish calls to the destroy timer functions, which I think should cause the algorithm to run smoother, but will need to test on monday

--- install/qbert_py_movement/lib/python3.10/site-packages/qbert_py_movement/wanderer.py
+++ install/qbert_py_movement/lib/python3.10/site-packages/qbert_py_movement/wanderer.py
@@ -30,20 +30,29 @@ class Wanderer(Node):
         go_back = Twist()
         go_back.linear.x = -0.01
         self.hazard_publisher.publish(go_back)
+        print("I backed up")
 
     def turn_callback(self):
         turn = Twist()
         turn.angular.z = 1.0
         self.hazard_publisher.publish(turn)
+        print("I'm turning")
 
     def destroy_back(self):
         if(self.back_timer):
             self.back_timer.destroy()
+        if(self.timer_stopper):
+            self.timer_stopper.destroy()
+        self.hazard_publisher.publish(Twist())
+        print("I stopped backing up")
 
     def destroy_turn(self):
         if(self.turn_timer):
             self.turn_timer.destroy()
-         
+        if(self.timer_stopper):
+             self.timer_stopper.destroy()
+        self.hazard_publisher.publish(Twist())
+        print("I stopped turning")
 
     def hazard_callback(self, haz):
         if len(haz.detections) > 0:
@@ -56,27 +65,34 @@ class Wanderer(Node):
                     self.back_timer = self.create_timer(.1, self.back_callback)
                     self.timer_stopper = self.create_timer(1, self.destroy_back)
                     time.sleep(1.5)
-                    print("turning")
+                    print("backing up")
                     turn_time = random.uniform(2,4)
                     self.turn_timer = self.create_timer(.1, self.turn_callback)
                     self.timer_stopper = self.create_timer(turn_time, self.destroy_turn)
+                    print("turning")
 
+    def tester(self):
+#        self.get_logger().info('hazard type!: "%i"' % haz.detections[i].type)
+        #print("bumped")
+        self.hazard_publisher.publish(Twist())
+        #print("stopped")
+        self.back_timer = self.create_timer(.1, self.back_callback)
+        self.timer_stopper = self.create_timer(1, self.destroy_back)
+        time.sleep(1.5)
+        print("backing up")
+        turn_time = random.uniform(2,4)
+        self.turn_timer = self.create_timer(.1, self.turn_callback)
+        self.timer_stopper = self.create_timer(turn_time, self.destroy_turn)
+        print("turning")
 
-
-                    
-                    
-
-                    #ros2 topic pub qbert/cmd_vel geometry_msgs/msg/Twist "{linear: {x: 0.1, y: 0.0, z: 0.0}, angular: {x: 0.0, y: 0.0, z: 0.0}}"
-
-
-
+#ros2 topic pub qbert/cmd_vel geometry_msgs/msg/Twist "{linear: {x: 0.1, y: 0.0, z: 0.0}, angular: {x: 0.0, y: 0.0, z: 0.0}}"
 def main(args=None):
     rclpy.init(args=args)
 
     wanderer = Wanderer()
-
+    
     rclpy.spin(wanderer)
-
+    wanderer.tester()
     # Destroy the node explicitly
     # (optional - otherwise it will be done automatically
     # when the garbage collector destroys the node object)
