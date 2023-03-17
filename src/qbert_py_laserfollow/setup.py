7edaafee8ea93543e785ae30bcd4a6d5de9271bf
Guruprasad Nayak <ice@wifi-roaming-128-4-186-71.wifi.udel.edu>
2023-03-17 09:51:53 -0400

    fixing files

--- src/qbert_py_laserfollow/setup.py
+++ src/qbert_py_laserfollow/setup.py
@@ -19,7 +19,7 @@ setup(
     license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
-        'console_scripts': [
+        'console_scripts': ['talker = qubert_py movement.laserfollow:main'
         ],
     },
 )
