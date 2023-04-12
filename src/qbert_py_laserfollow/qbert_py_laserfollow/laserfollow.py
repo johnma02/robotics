b98dd7de4806053a8e31e956ec2795cf2bea6913
John <ma.jonathan02@gmail.com>
2023-04-12 13:10:19 +0000

    add submission for qbert_py_laserfollow

--- src/qbert_py_laserfollow/qbert_py_laserfollow/laserfollow.py
+++ src/qbert_py_laserfollow/qbert_py_laserfollow/laserfollow.py
@@ -50,19 +50,49 @@ class Laserfollow(Node):
         increasing in intensity or decreasing
 
 
+        (1) Start in free space, go straight until a blocking wall is encountered in front (at a distance that depends on chosen offset).  
+        (2) If the chosen side = left, make a right turn at this wall and move forward parallel to it at the chosen offset.  If side = right, make a left turn and do the same
+        (3) If a blocking wall is encountered in front, turn away from side wall and continue parallel to blocking wall.  
+        (4) If the side wall is "lost," turn toward that side until a wall is refound, then continue forward
+
     """
-    
     def laser_callback(self, las):
-        if not following:
-            
         ranges = las.ranges
         left = len(ranges)//4
         right = (len(ranges))//4*3
         back = len(ranges)//2
+        left_front = len(ranges)//8
+        right_front = (len(ranges))//8*7
+        front = -1
 
-        print(f"front: {ranges[-1]}, left: {ranges[left]}, right: {ranges[right]}, back: {ranges[back]}")
+        if not self.following:
+            if ranges[front] < .5:
+                self.following = True
+            else:
+                forward = Twist()
+                forward.linear.x = .1
+                self.publisher.publish(forward)                
 
-
+        else:
+            twist = Twist()
+            if ranges[front] < 0.4:
+                twist.linear.x = .1
+                twist.angular.z = 0.05
+                self.publisher.publish(twist)
+            else:
+                twist.linear.x = .1
+                right_average = (ranges[right]+ranges[right+2]+ranges[right-2]+ranges[right-3] +ranges[right+3])/5
+                if right_average <= 0.4:
+                    twist.angular.z = 1.25 * (0.4-right_average)
+                self.publisher.publish(twist)
+                left_average = (ranges[left]+ranges[left+2]+ranges[left-2]+ranges[left-3] +ranges[left+3])/5
+                if left_average <= 0.4:
+                    twist.angular.z = -1.25 * (0.4-left_average)
+                print(f"front: {ranges[-1]}, left: {ranges[left]}, right: {right_average}") 
+                print(f"{self.following}")
+            if ranges[front] > 0.4:
+                twist.angular.z = 0.0
+                self.publisher.publish(twist)
 
 def main(args=None):
     rclpy.init(args=args)
